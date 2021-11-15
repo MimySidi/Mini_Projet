8e39034710b9da19e3beefbd371b17defb79b6a2
MimySidi <18019.sid.oumou@esp.mr>
2021-11-15 20:05:45 +0100

    mini

---
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.quizapp;
+
+import android.content.Intent;
+import android.support.v4.app.RemoteActionCompatParcelizer;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quizapp.QuestionActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startbutton=(Button)findViewById(R.id.button);
+        final EditText nametext=(EditText)findViewById(R.id.editName);
+
+        startbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=nametext.getText().toString();
+                Intent intent=new Intent(getApplicationContext(), QuestionActivity.class);
+                intent.putExtra("myname",name);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
